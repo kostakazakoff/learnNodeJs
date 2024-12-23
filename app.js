198be9865cc8683557294a1e4bd9f47f5d2542f2
Kosta Kazakov <kostakazakoff@gmail.com>
2024-12-23 03:11:58 +0200

    DB, schema, model

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const env = require('dotenv').config();
+const Blog = require('./models/blog');
 
 const app = express();
+
+// MongoDB connection URI
+const dbURI = process.env.MONGODBURI;
+mongoose.connect(dbURI)
+    .then(result => app.listen(3000))
+    .catch(err => console.error(err));
 
 // register view engine
 app.set('view engine', 'ejs');
@@ -9,24 +18,22 @@ app.set('view engine', 'ejs');
 // set views folder (when different from 'views')
 // app.set('view engine', 'views_folder');
 
-app.listen(3000);
-
 // middleware
 app.use(express.static('public')); //static files
 app.use(morgan('dev')); // logging requests
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('index', {
         title: 'Home',
         blogs: [
-            {title: 'Kosta', snippet: 'Kosta Alabala', body: 'Alabala'},
-            {title: 'Marina', snippet: 'Marina Alabala', body: 'Alabala'},
-            {title: 'Tedi', snippet: 'Tedi Alabala', body: 'Alabala'},
+            { title: 'Kosta', snippet: 'Kosta Alabala', body: 'Alabala' },
+            { title: 'Marina', snippet: 'Marina Alabala', body: 'Alabala' },
+            { title: 'Tedi', snippet: 'Tedi Alabala', body: 'Alabala' },
         ],
     });
 });
 
-app.get('/about', function(req, res) {
+app.get('/about', function (req, res) {
     res.render('about', {
         title: 'About'
     });
@@ -43,7 +50,17 @@ app.get('/blogs/create', (req, res) => {
     });
 });
 
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'New Blog',
+        snippet: 'about my new blog',
+        body: 'my new blog body'
+    });
+
+    blog.save();
+});
+
 // 404
 app.use((req, res) => {
-    res.status(404).render('404', {title: '404'});
+    res.status(404).render('404', { title: '404' });
 });
---
+++ models/blog.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create schema
+const blogSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    snippet: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    timestamps: true
+});
+
+// Create a model based on the schema
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = Blog;
